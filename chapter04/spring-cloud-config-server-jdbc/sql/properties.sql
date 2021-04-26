3ec3a3bfda9442802baac06499812fb6337f7841
Renjie Sun <sunrenjie6@gmail.com>
2021-04-26 09:25:39 +0800

    chapter04/spring-cloud-config-server-jdbc: to fix db configuration and SQL clauses
    1. Be determined and configure MySQL database credential.
    2. The default label null is changed to 'master', since the query code
       would translate the input parameter label value of null to 'master',
       causing a mismatch.

--- chapter04/spring-cloud-config-server-jdbc/sql/properties.sql
+++ chapter04/spring-cloud-config-server-jdbc/sql/properties.sql
@@ -8,8 +8,10 @@ CREATE TABLE `properties` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-
-
-INSERT INTO `properties` VALUES (null, 'book.name', 'deep in spring cloud', 'book', 'prod', null);
-INSERT INTO `properties` VALUES (null, 'book.author', 'jim', 'book', 'prod', null);
-INSERT INTO `properties` VALUES (null, 'book.category', 'spring cloud', 'book', 'prod', null);
+/*
+The label shall be 'master' instead of null, since null label will be converted to 'master' in
+org.springframework.cloud.config.server.environment.JdbcEnvironmentRepository.
+*/
+INSERT INTO `properties` VALUES (null, 'book.name', 'deep in spring cloud', 'book', 'prod', 'master');
+INSERT INTO `properties` VALUES (null, 'book.author', 'jim', 'book', 'prod', 'master');
+INSERT INTO `properties` VALUES (null, 'book.category', 'spring cloud', 'book', 'prod', 'master');
